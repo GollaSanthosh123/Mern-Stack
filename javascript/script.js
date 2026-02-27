b414fc0d5a227fbc2a00f6968a779c33a484a90c
santhosh <santhoshsanthu7228@gmail.com>
2026-02-27 16:05:57 +0530

    Java Script

---
+++ javascript/script.js
@@ -0,0 +1,44 @@
+// console.log("Hello World!!!!")
+// var a;//globel scope
+// a=9;
+// var a;
+// a=1;
+// let b=8;//block scope
+//  b=9;
+//  const c=3.14;//block
+// console.log(a);
+// console.log(b);
+// console.log(a+b);
+// var str="santhosh";//``,'',""
+// console.log(str);
+// console.log(typeof str);
+// var bool=true;
+// var un;//undefined
+// var nu=null;
+// var big=123455667n;
+// var symbol=Symbol('li');
+// console.log(typeof big)
+// //arr
+// var arr=[1,2,3];
+// console.log(arr);
+//dict--object
+// var obj={name:"santhu",roll:"D8"};
+// console.log(obj);
+
+// Arithematic operatinos
+var a=10;
+var b=2;
+console.log(a/b);
+console.log(a**b);
+
+//relational
+b=10;
+console.log(a>=b);
+console.log(a==b);
+b="10";
+console.log(a===b); // check values and datatypes alsooo.
+//logical operators
+var b1=true;
+var b2=false;
+console.log(b1||b2);
+console.log(!b1)
